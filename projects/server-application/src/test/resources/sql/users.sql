bf7f8dd07a7a033e7c6801b7a56665b9463608eb
Hurbanov Tamerlan <t64566t@gmail.com>
2024-05-04 20:25:31 +0300

    fix: fixed broken sequence start value for data

--- projects/server-application/src/test/resources/sql/users.sql
+++ projects/server-application/src/test/resources/sql/users.sql
@@ -1,3 +1,6 @@
+-- Use Explicit id values to simplify referencing by id in data
 INSERT INTO users (id, email, first_name, last_name, birth_date, address, phone_number, created_at, last_modified_at)
 VALUES (1, 'johndoe@gmail.com', 'John', 'Doe', '2002-02-22', 'John Doe address','+1234567892', '2024-04-24T22:22:09.266615Z',
         '2024-04-24T22:28:19.266615Z');
+-- Reset sequence to avoid conflicts with next inserts
+ALTER SEQUENCE users_id_seq RESTART WITH 2;
